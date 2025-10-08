76f59e9eb50a8f9f858e7f17d7d31d5a82cc576c
balnc9 <matthewlchin1@gmail.com>
2025-10-08 17:26:41 +0200

    test setup

---
+++ test.py
@@ -0,0 +1,25 @@
+import os
+'''
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms,modes
+
+
+key = os.urandom(32)
+iv = os.urandom(16)
+
+cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+encryptor = cipher.encryptor()
+ct = encryptor.update(b"Secret Message #1") + encryptor.finalize()
+decryptor = cipher.decryptor()
+dec = decryptor.update(ct) + decryptor.finalize()
+
+print(dec)
+'''
+
+
+from cryptography.fernet import Fernet
+key = Fernet.generate_key()
+f = Fernet(key)
+tok = f.encrypt(b"secret message 2")
+print(tok)
+
+print(f.decrypt(tok))
