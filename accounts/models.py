b0d97d0db261b4e38afecaa74b0bf4ef9741ad0d
jiho lee <jiholee@jihos-MacBook-Air-2.local>
2025-10-29 17:55:38 +0100

    implemented crypto stuff on account generation in manager.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,18 +4,40 @@ import uuid
 
 @dataclass(frozen=True)
 class User:
+    # basic account information
     user_id: str
     username: str   # canonical (e.g., lowercased)
     pwd_hash: str
     created_at: str   # ISO8601 "YYYY-MM-DDTHH:MM:SSZ"
 
+    # encryption stuff
+    public_key: str
+    enc_private_key: str
+    enc_private_key_nonce: str
+    enc_private_key_salt: str
+    key_wrap_version: str = "v1"
+
+    # constructor
     @staticmethod
-    def new(username: str, pwd_hash: str) -> "User":
-        now = now = datetime.now(timezone.utc).isoformat(timespec="seconds").replace("+00:00", "Z")
+    def new(
+        username: str,
+        pwd_hash: str,
+        public_key: str,
+        enc_private_key: str,
+        enc_private_key_nonce: str,
+        enc_private_key_salt: str,
+        key_wrap_version: str = "v1",
+    ) -> "User":
+        now = datetime.now(timezone.utc).isoformat(timespec="seconds").replace("+00:00", "Z")
 
         return User(
             user_id=str(uuid.uuid4()),
             username=username,
             pwd_hash=pwd_hash,
             created_at=now,
+            public_key=public_key,
+            enc_private_key=enc_private_key,
+            enc_private_key_nonce=enc_private_key_nonce,
+            enc_private_key_salt=enc_private_key_salt,
+            key_wrap_version=key_wrap_version,
         )
